b5c08b0f0ff15e5a259df88dbc1b86422647fe4d
jhasuraj01 <44930179+jhasuraj01@users.noreply.github.com>
2021-09-28 19:23:17 +0000

    updated

--- pages/index.jsx
+++ pages/index.jsx
@@ -61,11 +61,13 @@ export async function getStaticProps() {
 //   const res = await fetch(`/api/hello`)
 //   const data = await res.json()
 
-    const date = new Date()
-    const time = date.toLocaleString('en-IN')
+    const currentTime = new Date();
+    const currentOffset = currentTime.getTimezoneOffset();
+    const ISTOffset = 330;   // IST offset UTC +5:30 
+    const ISTTime = new Date(currentTime.getTime() + (ISTOffset + currentOffset)*60000);
 
     const data = {
-        date: time
+        date: ISTTime.toString()
     }
 
   if (!data) {
