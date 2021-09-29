a73d4e74e1a3219f9ccdfe1a2be86acbec3abef2
jhasuraj01 <44930179+jhasuraj01@users.noreply.github.com>
2021-09-29 00:37:06 +0000

    New Auto Build

---
+++ docs/next/static/vDSFluRjk15IOp5OLUIGs/buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e26d3648d4908922378b.css","static/chunks/pages/index-72a32d416c5a65666e0e.js"],"/_error":["static/chunks/pages/error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- docs/next/static/vN6-UFsJdrye0MG2tyGCr/buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/60efdbbfef958fb6fb48.css","static/chunks/pages/index-ecf07ac6212e88eab78f.js"],"/_error":["static/chunks/pages/error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
