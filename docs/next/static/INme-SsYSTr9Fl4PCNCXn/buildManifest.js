c71382ccec245ee7a0707f73ebb1db8f71be4d60
jhasuraj01 <44930179+jhasuraj01@users.noreply.github.com>
2021-09-29 00:11:14 +0000

    New Auto Build

---
+++ docs/next/static/INme-SsYSTr9Fl4PCNCXn/buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4298862328a59a789b19.css","static/chunks/pages/index-ecf07ac6212e88eab78f.js"],"/_error":["static/chunks/pages/error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- docs/next/static/R7UIc8e_lCCPEzq4YdKza/buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4298862328a59a789b19.css","static/chunks/pages/index-17e28f4f5690f30ca9d6.js"],"/_error":["static/chunks/pages/error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
