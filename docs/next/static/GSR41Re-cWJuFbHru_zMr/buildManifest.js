a723e481e917229931c72d83d98dacf16e6e0027
jhasuraj01 <44930179+jhasuraj01@users.noreply.github.com>
2021-09-29 01:15:24 +0000

    New Auto Build

---
+++ docs/next/static/GSR41Re-cWJuFbHru_zMr/buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e26d3648d4908922378b.css","static/chunks/pages/index-cb64e97cace3017960d1.js"],"/_error":["static/chunks/pages/error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
