826827825f94ddd907eeda05b462b00db32ec7e6
jhasuraj01 <44930179+jhasuraj01@users.noreply.github.com>
2021-09-29 00:27:53 +0000

    New Auto Build

--- docs/next/static/z2xEHKtJS3mbSWKcWmWRC/buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/7ad006f57157251f5f17.css","static/chunks/pages/index-ecf07ac6212e88eab78f.js"],"/_error":["static/chunks/pages/error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
